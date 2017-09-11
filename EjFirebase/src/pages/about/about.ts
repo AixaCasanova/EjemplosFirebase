7f43e20dcf2484d078eb0566039185628fc2aba3
Aixa <aixacasanova@gmail.com>
2017-09-11 18:58:13 -0300

    ejemplo a completar

--- EjFirebase/src/pages/about/about.ts
+++ EjFirebase/src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  dato: FirebaseListObservable<any[]>;
+  persona={
+    nombre:"",
+    edad:""
+  };
 
+  constructor(public navCtrl: NavController, af: AngularFireDatabase) {
+     
   }
 
+  agregarPersona()
+  {}
 }
--- EjFirebase/src/pages/home/home.ts
+++ EjFirebase/src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular'; 
-// import {firebaseconfig} from '../firebase/';
+import { NavController } from 'ionic-angular';  
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
